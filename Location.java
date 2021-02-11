2b70e0d57c4cfcd934186ca3a09410abec006950
Jotrol <ilichevdaniil@gmail.com>
2021-02-12 02:48:14 +0300

    fixed lags

---
+++ Location.java
@@ -0,0 +1,39 @@
+/**
+ * This class represents a specific location in a 2D map.  Coordinates are
+ * integer values.
+ **/
+public class Location
+{
+    /** X coordinate of this location. **/
+    public int xCoord;
+
+    /** Y coordinate of this location. **/
+    public int yCoord;
+
+
+    /** Creates a new location with the specified integer coordinates. **/
+    public Location(int x, int y)
+    {
+        xCoord = x;
+        yCoord = y;
+    }
+
+    /** Creates a new location with coordinates (0, 0). **/
+    public Location()
+    {
+        this(0, 0);
+    }
+
+    public boolean equals(Location loc)
+    {
+        return this.xCoord == loc.xCoord && this.yCoord == loc.yCoord;
+    }
+    public int hashCode()
+    {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + xCoord;
+        result = prime * result + yCoord;
+        return result;
+    }
+}
